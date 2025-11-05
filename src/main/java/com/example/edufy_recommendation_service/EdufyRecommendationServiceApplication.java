8778a654c30f60ae1b1d5c05989e54a9122042c1
kinhenrik <hkin98@gmail.com>
2025-11-05 10:22:52 +0100

    Initial commit

---
+++ src/main/java/com/example/edufy_recommendation_service/EdufyRecommendationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.edufy_recommendation_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdufyRecommendationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdufyRecommendationServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/edufy_recommendation_service/EdufyRecommendationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.edufy_recommendation_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdufyRecommendationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
